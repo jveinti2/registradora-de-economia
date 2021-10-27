f7553db7e6783cf23259e1a3aac40b34e0b302c2
Jhonatan Becerra <jsbpdeveloper@gmail.com>
2021-10-26 20:50:04 -0500

    Agregando funcionalidad de agregar datos a la tabla

--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Datos guardados')
+    let formData = new FormData(form);
+    let tablaTransaccionesRef = document.getElementById("tabla_transacciones");
+    let newRow = tablaTransaccionesRef.insertRow(-1);
+    let newCell = newRow.insertCell(0);
+    newCell.textContent = formData.get("tipo");
+
+    newCell = newRow.insertCell(1);
+    newCell.textContent = formData.get("descripcion");
+    
+    newCell = newRow.insertCell(2);
+    newCell.textContent = formData.get("monto");
+    
+    newCell = newRow.insertCell(3);
+    newCell.textContent = formData.get("categoria");
+    alert("Datos agregados");
+    console.log("funciona la logica");
+    resetForm();
 })
+
+function resetForm () {
+    form.reset();
+}
